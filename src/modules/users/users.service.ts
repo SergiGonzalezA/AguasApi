0a11cab05ef473c1244e665354f4f019b6f86728
AJRego <ajrego@evoluciondigital.com.co>
2024-08-29 20:24:32 -0500

    feat: add validations to users service to improve the code

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { User } from 'src/schemas/user.schema';
 
 @Injectable()
@@ -19,16 +23,37 @@ export class UsersService {
   }
 
   async findOne(id: string): Promise<User> {
-    return this.userModel.findById(id).exec();
+    this.validateObjectId(id);
+    const user = await this.userModel.findById(id).exec();
+    if (!user) {
+      throw new NotFoundException(`User with ID "${id}" not found`);
+    }
+    return user;
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-    return this.userModel
+    this.validateObjectId(id);
+    const updatedUser = await this.userModel
       .findByIdAndUpdate(id, updateUserDto, { new: true })
       .exec();
+    if (!updatedUser) {
+      throw new NotFoundException(`User with ID "${id}" not found`);
+    }
+    return updatedUser;
   }
 
   async remove(id: string): Promise<User> {
-    return this.userModel.findByIdAndDelete(id).exec();
+    this.validateObjectId(id);
+    const deletedUser = await this.userModel.findByIdAndDelete(id).exec();
+    if (!deletedUser) {
+      throw new NotFoundException(`User with ID "${id}" not found`);
+    }
+    return deletedUser;
+  }
+
+  private validateObjectId(id: string) {
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException(`Invalid ObjectId: "${id}"`);
+    }
   }
 }
