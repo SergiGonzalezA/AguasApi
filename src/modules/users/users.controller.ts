86f757aff444c1cda5e0a8d72b981eb72c0a74ef
AJRego <ajrego@evoluciondigital.com.co>
2024-09-01 02:38:50 -0500

    add new endpoint /import to save users from a csv file

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,10 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as csv from 'csv-parser';
+import { Readable } from 'stream';
 
 @Controller('api/v1/users')
 export class UsersController {
@@ -38,5 +43,42 @@ export class UsersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
+  }
+
+  @Post('import')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return new Promise((resolve, reject) => {
+      const users: CreateUserDto[] = [];
+  
+      const bufferStream = new Readable();
+      bufferStream.push(file.buffer);
+      bufferStream.push(null);
+  
+      bufferStream
+        .pipe(csv())
+        .on('data', (row) => {
+          const rowValues = Object.values(row);
+          const user: CreateUserDto = {
+            identification: typeof rowValues[0] === 'string' ? rowValues[0].trim() : '',
+            fullName: typeof rowValues[1] === 'string' ? rowValues[1].trim().toUpperCase() : '',
+            phone: typeof rowValues[2] === 'string' ? rowValues[2].trim() : '',
+            pendingBalance: 0,
+            debtMonths: 0,
+            isActive: true
+          };
+  
+          console.log('user csv creado:', user);
+          users.push(user);
+        })
+        .on('end', async () => {
+          try {
+            await this.usersService.bulkCreate(users);
+            resolve({ message: 'archivo procesado correctamente' });
+          } catch (error) {
+            reject(error);
+          }
+        });
+    });
   }
 }
