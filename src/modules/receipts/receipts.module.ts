a8004c8b506d4fabc70106f40533ad5211e656f5
AJRego <ajrego@evoluciondigital.com.co>
2024-09-06 11:20:57 -0500

    feat: add payment model to receipts module

--- src/modules/receipts/receipts.module.ts
+++ src/modules/receipts/receipts.module.ts
@@ -3,10 +3,14 @@ import { ReceiptsService } from './receipts.service';
 import { ReceiptsController } from './receipts.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/user.schema';
+import { Payment, PaymentSchema } from 'src/schemas/payments.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Payment.name, schema: PaymentSchema },
+    ]),
   ],
   controllers: [ReceiptsController],
   providers: [ReceiptsService],
